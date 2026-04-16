cd54961a0c6f9db2b00a551eb7420d0e38964963
Madubuike Favour Ugochukwu <ezegeone198@gmail.com>
2026-04-16 11:55:05 +0100

    Update string.js

--- src/string.js
+++ src/string.js
@@ -3,7 +3,6 @@ chore: add .gitignore for node_modules and dist
 chore: add package.json with project metadata
 docs: write project description in README
 chore: add .eslintrc.json with base rules
-chore 
-
+ 
 
  
