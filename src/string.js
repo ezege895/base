39055f5e8234d782dec8aac0136622b139394981
Madubuike Favour Ugochukwu <ezegeone198@gmail.com>
2026-04-16 11:58:56 +0100

    Update string.js

--- src/string.js
+++ src/string.js
@@ -1,6 +1,6 @@
 chore: initial commit
 chore: add .gitignore for node_modules and dist
-chore: add package.json with project metadata
+chore: add pac with project metadata
  ct description in README
  
 
